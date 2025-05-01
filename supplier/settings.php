573c45609e541d18533682b32d06cd10c5d6e405
Robbe Decapmaker <debber@dcpm.be>
2025-05-01 22:04:20 +0200

    Get db settings from env

--- supplier/settings.php
+++ supplier/settings.php
@@ -1,8 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Nalu123!";
-$dbname = "dapp";
+// try to get settings from environment (for opsec), otherwise use dev settings.
+// IMPORTANT: Never use the standard settings in production, if that was not obvious already
+$servername = getenv('SUP_SERVERNAME');
+if (!$servername) {
+  $servername = "localhost";
+}
+$username = getenv('SUP_USERNAME');
+if (!$username) {
+  $username = "root";
+}
+$password = getenv('SUP_PASSWORD');
+if (!$password) {
+  $password = "Nalu123!";
+}
+$dbname = getenv('SUP_DBNAME');
+if (!$dbname) {
+  $dbname = "dapp";
+}
+
+
 // Create connection
 try {
   $conn = mysqli_connect($servername, $username, $password, $dbname);
